c7968a6f3a048123df7d4e772c7241e3d4fee3ed
Jindra Helcl <helcl@ufal.mff.cuni.cz>
2017-01-27 16:13:31 +0100

    recurrent attention

--- neuralmonkey/encoders/sentence_encoder.py
+++ neuralmonkey/encoders/sentence_encoder.py
@@ -36,6 +36,7 @@ class SentenceEncoder(ModelPart, Attentive):
                  max_input_len: int,
                  embedding_size: int,
                  rnn_size: int,
+                 attention_state_size: int,
                  dropout_keep_prob: float=1.0,
                  attention_type: Optional[AttType]=None,
                  attention_fertility: int=3,
@@ -67,7 +68,8 @@ class SentenceEncoder(ModelPart, Attentive):
         """
         ModelPart.__init__(self, name, save_checkpoint, load_checkpoint)
         Attentive.__init__(
-            self, attention_type, attention_fertility=attention_fertility)
+            self, attention_type, attention_fertility=attention_fertility,
+            attention_state_size=attention_state_size)
 
         assert check_argument_types()
 
